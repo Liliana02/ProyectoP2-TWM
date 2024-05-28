2cfea266b70f6a726fa048f5e161442045898e5b
Liliana Lozano <LRS20110117@purisima.tecnm.mx>
2024-05-28 03:38:55 -0600

    Correcciones

--- AguadeCactus.WebSite/Pages/User/Login.cshtml.cs
+++ AguadeCactus.WebSite/Pages/User/Login.cshtml.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AguadeCactus.Api.Dto;
+using AguadeCactus.Core.Http;
+using AguadeCactus.WebSite.Services;
 
 namespace AguadeCactus.WebSite.Pages.User;
 
 public class Login : PageModel
 {
-    public void OnGet()
+    [BindProperty] public UserDto UserDto { get; set; }
+
+    public List<string> Errors { get; set; } = new List<string>();
+    
+    private readonly IUserService _service;
+    public async Task<IActionResult> OnGet()
     {
-        
+        return Page();
     }
+
+    
 }
