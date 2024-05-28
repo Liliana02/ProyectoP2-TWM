9c4f4b1ca67e4de5eea9cfd776e4e8aa7ee595c2
Liliana Lozano <LRS20110117@purisima.tecnm.mx>
2024-05-28 01:12:42 -0600

    Menú completo

--- AguadeCactus.WebSite/Pages/ListProductPromotion.cshtml.cs
+++ AguadeCactus.WebSite/Pages/ListProductPromotion.cshtml.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AguadeCactus.Api.Dto;
+using AguadeCactus.Core.Http;
+using AguadeCactus.WebSite.Services;
 
 namespace AguadeCactus.WebSite.Pages.Product;
 
 public class ListProductPromotion : PageModel
 {
-    public void OnGet()
+    private readonly IProductService _serviceP;
+    private readonly IPromotionService _servicePr;
+    
+    public List<ProductDto> Products { get; set; }
+    public List<PromotionDto> Promotions { get; set; }
+
+    public ListProductPromotion(IProductService serviceP, IPromotionService servicePr)
     {
-        
+        Products = new List<ProductDto>();
+        Promotions = new List<PromotionDto>();
+        _serviceP = serviceP;
+        _servicePr = servicePr;
+    }
+    public async Task<IActionResult> OnGet()
+    {
+        var responseP = await _serviceP.GetAllAsync();
+        var responsePr = await _servicePr.GetAllAsync();
+        Products = responseP.Data;
+        Promotions = responsePr.Data;
+
+        return Page();
     }
 }
