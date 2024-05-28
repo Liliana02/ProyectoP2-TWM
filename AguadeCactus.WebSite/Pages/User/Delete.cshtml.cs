1106cfab410a98db966a47c9dd1e91b835105344
Liliana Lozano <LRS20110117@purisima.tecnm.mx>
2024-05-27 18:13:28 -0600

    Edita/Eliminar usuarios página editar

--- AguadeCactus.WebSite/Pages/User/Delete.cshtml.cs
+++ AguadeCactus.WebSite/Pages/User/Delete.cshtml.cs
@@ -1,11 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AguadeCactus.Api.Dto;
+using AguadeCactus.WebSite.Services;
 
 namespace AguadeCactus.WebSite.Pages.User;
 
 public class Delete : PageModel
 {
-    public void OnGet()
+    [BindProperty] public UserDto UserDto { get; set; }
+
+    public List<string> Errors { get; set; } = new List<string>();
+    private readonly IUserService _service;
+
+    public Delete(IUserService service)
     {
-        
+        _service = service;
+    }
+    public async Task<IActionResult> OnGet(int id)
+    {
+        UserDto = new UserDto();
+        var response = await _service.GetById(id);
+        UserDto = response.Data;
+
+        if (UserDto == null)
+        {
+            return RedirectToPage("/Error");
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var response = await _service.DeleteAsync(UserDto.id);
+        return RedirectToPage("./Edit");
     }
 }
